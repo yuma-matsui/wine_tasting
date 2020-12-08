e73edf6d4ed3927f338631ff044cab5439412f50
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-08 15:37:15 +0900

    会員登録ページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true, uniqueness: true, format: { with: /\A[a-z\d]{4,}+\z/i }
+  validates :email, uniqueness: true, format: { with: /.+@.+/ }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
 end
