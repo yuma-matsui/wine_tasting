0a8ba436893f90c9285f337760c72d14b0b2562d
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-10 13:30:25 +0900

    ノート作成ページへのリンク作成

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,5 +2,8 @@ class WinesController < ApplicationController
 
   def home
   end
+
+  def new
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'wines#home'
+  resources :wines, only: [:new] 
 end
