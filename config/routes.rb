641734dccb3c13e5099c9d252dcf8016298ab157
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-12 13:53:20 +0900

    edit、destroyアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'wines#home'
-  resources :wines, only: [:new, :create, :edit, :destroy, :update] 
+  resources :wines, only: [:new, :create, :edit, :destroy, :update, :show] 
 end
