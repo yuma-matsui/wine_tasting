7c4aac6d88cbbed06935ba5ac2d131c5cd18e9d5
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-14 21:53:58 +0900

    favoritesコントローラーのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'wines#home'
-  resources :wines, only: [:new, :create, :edit, :destroy, :update, :show] 
+  resources :wines, only: [:new, :create, :edit, :destroy, :update, :show] do
+    resources :favorites, only: [:create, :destroy]
+  end
 end
