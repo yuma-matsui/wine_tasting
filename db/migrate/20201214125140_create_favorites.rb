ccd7a48533156c4d4300f43420a36d11cc8d9931
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-15 20:19:15 +0900

    favoriteテーブルの外部キー設定

--- db/migrate/20201214125140_create_favorites.rb
+++ db/migrate/20201214125140_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-      t.references :user, null: false
-      t.references :wine, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :wine, null: false, foreign_key: true
       t.timestamps
     end
   end
